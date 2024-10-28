cbb3d2269c3f1588dd8f7052144ba2014ae6f2e4
tthanh25 <21020790@vnu.edu.vn>
2024-10-28 12:27:48 +0700

    data_preparation_detection and MSCN change

--- data_preparation_detection.py
+++ data_preparation_detection.py
@@ -1,34 +1,32 @@
-import skimage.transform
-import PIL
-import scipy.misc
+import numpy as np
 import skimage
 import imageio
 from MSCN import calculate_brisque_features
 import tensorflow as tf
 from PGD_attack import pgd
 
+# Load MNIST dataset
 mnist = tf.keras.datasets.mnist
-(_,_),(x_test,_) = mnist.load_data()
+(_, _), (x_test, _) = mnist.load_data()
 
-x_test = np.reshape(x_test,(-1,28,28,1))
+# Reshape the dataset
+x_test = np.reshape(x_test, (-1, 28, 28, 1))
 
+# Prepare the feature matrix
 for i in range(1000):
-
-        img = x_test[i]
-        img=pgd(img,i)
-        parameters=calculate_brisque_features(img, kernel_size=7, sigma=7/6)
-        t = np.hstack((1, parameters))
-        if i == 0:
-            v = t
-        else:
-            v = np.vstack((v, t))
-
-
-        img = x_test[i]
-        parameters=calculate_brisque_features(img, kernel_size=7, sigma=7/6)
-        t = np.hstack((0, parameters))
+    img = x_test[i]
+    img = pgd(img, i)
+    parameters = calculate_brisque_features(img, kernel_size=7, sigma=7/6)
+    t = np.hstack((1, parameters))  # Label for adversarial image
+    if i == 0:
+        v = t
+    else:
         v = np.vstack((v, t))
 
+    img = x_test[i]
+    parameters = calculate_brisque_features(img, kernel_size=7, sigma=7/6)
+    t = np.hstack((0, parameters))  # Label for clean image
+    v = np.vstack((v, t))
 
 print(np.shape(v))
-np.savez_compressed('data_training', X=v[:, 1:], Y=v[:, 0: 1])
+np.savez_compressed('data_training', X=v[:, 1:], Y=v[:, 0:1])
